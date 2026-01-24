db56672114392ebcf9c34337deb69547b384058b
CozySL <cocoonsayprachanh@gmail.com>
2026-01-24 15:39:34 +0300

    implement necessary elements (will refactor the codes ater)

---
+++ src/js/keyHandler.js
@@ -0,0 +1,101 @@
+import { setScreen } from '/js/main.js';
+
+
+const cmdLine = document.getElementById('cmdSearch');
+const cmdInput = document.getElementById('search_command');
+const isHidden = cmdLine.style.display === 'none' || cmdInput.style.display === '';
+
+
+//text (part for writing)
+const EditorSection = document.querySelector('.editor-container');
+const CodeAreaFocus = document.getElementById('editable-text'); // get the id of the code's area
+
+
+const DefaultMode = 'Normal mode';
+const cmdMode = 'command mode';
+const instMode = 'insert mode';
+let CurrentMode = DefaultMode;
+
+// normal mode (for waiting for keybind)
+console.log({CurrentMode});
+
+export function handlerModePress(e){
+  
+  if(CurrentMode === DefaultMode){
+    if(e.key === ':'){ // command mode
+    e.preventDefault();
+    EnterCommandMode();
+    
+
+    }
+    if(e.key === 'i'){ //insert mode
+    e.preventDefault();
+    setScreen('editor');
+    EnterInsertMode();
+  
+    }
+  }
+
+  //grobal exit
+  if(e.key === 'Escape'){
+    e.preventDefault(); // prevent the letter being write in the text
+    exitMode();
+  }
+
+};
+
+
+function exitMode(){
+  if(cmdInput && cmdLine){
+    cmdLine.style.display = 'none';
+    cmdInput.blur();
+    cmdInput.value = '';
+  }
+  //EditorSection.style.display = 'block';
+    EditorSection.ariaReadOnly = true;
+  
+  CurrentMode = DefaultMode;
+  console.log({CurrentMode});
+};
+
+
+// command mode
+function EnterCommandMode(){
+
+  if (isHidden){
+    cmdLine.style.display = 'block';
+    cmdInput.focus();
+    cmdInput.value = ':';
+    
+    //(test code!!) might update late / handling + storing command
+    cmdInput.addEventListener('keydown', (e)=>{
+      if(e.key === 'Enter'){
+        const cmd = cmdInput.value.trim();
+        if(cmd === ':home'){
+          setScreen('welcome');
+          exitMode();
+        }
+        cmdInput.value = '';
+      }
+
+    });
+
+
+  }
+  CurrentMode = cmdMode;
+  console.log({CurrentMode});
+
+};
+
+// insert mode
+function EnterInsertMode(){
+
+  EditorSection.style.display = 'flex';
+  CurrentMode = instMode;
+  
+  CodeAreaFocus.focus(); // focus on the code when enter insert mode
+
+  console.log({CurrentMode});
+}
+
+
---
+++ src/js/main.js
@@ -0,0 +1,67 @@
+import { handlerModePress } from '/js/keyHandler.js';
+
+
+const { invoke } = window.__TAURI__.core;
+
+let greetInputEl;
+let greetMsgEl;
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    greet();
+  });
+});
+
+//'my code' section
+
+
+// line-counting function
+const CodeArea = document.getElementById('editable-text');
+const NumberLine = document.getElementById('numLine');
+
+
+function updateLine(){
+  const lineCount = CodeArea.value.split('\n').length || 1;
+  
+  let numHTML = '';
+  for (let i = 1 ; i <= lineCount; i++) {
+    numHTML += i + '<br>';
+  }
+  
+  NumberLine.innerHTML = numHTML;
+}
+
+CodeArea.addEventListener('input', updateLine);
+
+//handling keymap
+document.addEventListener('keydown', (e) =>{
+  handlerModePress(e);
+});
+
+
+// screen | mode render
+const AppState = {
+  screen: 'welcome', mode:'normal'
+};
+
+
+
+export function setScreen(screen){
+  AppState.screen = screen;
+
+  document.querySelectorAll('.screen').forEach(s => s.classList.remove('active'));
+  document.getElementById(screen).classList.add('active');
+}
+
+//setScreen('welcome'); 
+
+ 
+
